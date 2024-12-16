b829485ff33531ded4552fb9c2391ccfd1a6e5f3
Pinaki <pinakiprasadrajkumar@gmail.com>
2024-12-16 14:41:10 +0530

    new changes

--- src/components/IndividualDonSpon/index.jsx
+++ src/components/IndividualDonSpon/index.jsx
@@ -118,7 +118,7 @@ const IndividualDonSpon = () => {
                             </p>
                         </div> */}
                         <p className="sponsorship-description">
-                            {donation.description || "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}
+                            {donation.eventDescription || "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}
                         </p>
                         <div className="ids-amount">
                                  <p style={{ marginTop: '1rem', marginBottom: '0rem', color: '#174873', fontSize: '15px', fontWeight: '500' }}>Total Amount:-(₹) {donation.amount ? donation.amount : donation.sponsorshipAmount}</p>
